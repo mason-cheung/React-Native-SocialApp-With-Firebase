a274d07df7e561ba13145aab64bf1cca023b0119
masonAIT <masoncch@users.noreply.github.com>
2020-09-23 06:03:22 +1000

    Added create user function

--- Config/Fire.js
+++ Config/Fire.js
@@ -13,7 +13,7 @@ class Fire {
     }
 
     addPost = async ({text, localUri}) => {
-        const remoteUri = await this.uploadPhotoAsync(localUri)
+        const remoteUri = await this.uploadPhotoAsync(localUri, `photos/${this.uid}/${Date.now()}`)
 
         return new Promise((res, rej) => {
             this.firestore.collection('Posts').add({
@@ -31,14 +31,13 @@ class Fire {
         });
     }
 
-    uploadPhotoAsync = async uri => {
-        const path = `photos/${this.uid}/${Date.now()}.jpg`
+    uploadPhotoAsync = async (uri, filename) => {
 
         return new Promise(async(res, rej) => {
             const response = await fetch(uri)
             const file = await response.blob()
 
-            let upload = firebase.storage().ref(path).put(file)
+            let upload = firebase.storage().ref(filename).put(file);
 
             upload.on('state_changed', snapshot => {}, err => {
                 rej(err)
@@ -49,6 +48,37 @@ class Fire {
             }
             )
         })
+    }
+
+    createUser = async user => {
+        let remoteUri = null
+
+        try {
+          await firebase
+            .auth()
+            .createUserWithEmailAndPassword(user.email, user.password);
+          let db = this.firestore.collection("users").doc(this.uid);
+
+          db.set({
+            name: user.name,
+            email: user.email,
+            avatar: null,
+          });
+
+          if (user.avatar) {
+            remoteUri = await this.uploadPhotoAsync(
+              user.avatar,
+              `avatars/${this.uid}`
+            );
+            db.set({ avatar: remoteUri }, { merge: true });
+          }
+        } catch (error) {
+          alert("Error: ", error);
+        }
+    }
+
+    signOut = () => {
+        firebase.auth().signOut();
     }
 
     get firestore() {
