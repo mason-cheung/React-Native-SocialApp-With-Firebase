bf801eb585fa9777e358287c45bcd61722e09b48
Mason <mason.chunho@gmail.com>
2020-09-25 22:38:33 +0800

    Created function to fetch data from firestore but not working properly

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,17 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
-import moment from 'moment'
-import Fire from '../Config/Fire'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import moment from "moment";
+import Fire from "../Config/Fire";
+import * as firebase from "firebase";
 
 // Temporary list data
 const posts = [
@@ -13,8 +21,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage1.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage1.jpg"),
   },
   {
     id: "2",
@@ -22,8 +30,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage2.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage2.jpg"),
   },
   {
     id: "3",
@@ -31,8 +39,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage3.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage3.jpg"),
   },
   {
     id: "4",
@@ -40,8 +48,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage4.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage4.jpg"),
   },
   {
     id: "5",
@@ -49,8 +57,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage1.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage1.jpg"),
   },
   {
     id: "6",
@@ -58,8 +66,8 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage2.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage2.jpg"),
   },
   {
     id: "7",
@@ -67,32 +75,47 @@ const posts = [
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
     timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage3.jpg')
+    avatar: require("../assets/tempAvatar.jpg"),
+    image: require("../assets/tempImage3.jpg"),
   },
-  {
-    id: "8",
-    name: "Victorie",
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, leo id vulputate scelerisque, enim justo ultrices eros, quis pretium ipsum ipsum sit amet enim. Ut non eleifend eros. Sed imperdiet tellus eget aliquam vehicula. Vestibulum ut rutrum nibh. Nunc blandit vel purus vel dictum. Vivamus sed egestas nibh. Suspendisse potenti.",
-    timestamp: 1600646400123,
-    avatar: require('../assets/tempAvatar.jpg'),
-    image: require('../assets/tempImage4.jpg')
-  },
-
 ];
-
 
 // create a component
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listPost: [],
+      avatar: this.avatar,
+      name: this.name,
+    };
+    this.ref = firebase.firestore().collection("Posts");
+  }
   static navigationOptions = {
-    headerShown: false
+    headerShown: false,
+  };
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot((querySnapshot) => {
+      const post = [];
+      querySnapshot.forEach((doc) => {
+        post.push({
+          postId: doc.data().uid,
+          postText: doc.data().text,
+          ...doc.data(),
+        });
+      });
+      this.setState({
+        listPost: post,
+        loading: false,
+      });
+    });
   }
 
-  renderPost = post => {
+  renderPost = (listPost) => {
     return (
       <View style={styles.feedItem}>
-        <Image source={post.avatar} style={styles.avatar} />
+        <Image source={listPost.avatar} style={styles.avatar} />
         <View style={{ flex: 1 }}>
           <View
             style={{
@@ -102,21 +125,32 @@ class HomeScreen extends Component {
             }}
           >
             <View>
-              <Text style={styles.name}>{post.name}</Text>
-              <Text style={styles.timestamp}>{moment(post.timestamp).fromNow()}</Text>
+              <Text style={styles.name}>{listPost.uid}</Text>
+              <Text style={styles.timestamp}>
+                {moment(listPost.timestamp).fromNow()}
+              </Text>
             </View>
-            <Ionicons name='ios-more' size={24} color='#73788B' />
+            <Ionicons name="ios-more" size={24} color="#73788B" />
           </View>
-          <Text style={styles.post}>{post.text}</Text>
-          <Image source={post.image} style={styles.postImage} resizeMode='cover' />
-          <View style={{ flexDirection: 'row' }}>
-            <Ionicons name='ios-heart-empty' size={24} color='#73788B' style={{ marginRight: 16 }} />
-            <Ionicons name='ios-chatboxes' size={24} color='#73788B' />
+          <Text style={styles.post}>{listPost.postText}</Text>
+          <Image
+            source={listPost.postImage}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <View style={{ flexDirection: "row" }}>
+            <Ionicons
+              name="ios-heart-empty"
+              size={24}
+              color="#73788B"
+              style={{ marginRight: 16 }}
+            />
+            <Ionicons name="ios-chatboxes" size={24} color="#73788B" />
           </View>
         </View>
       </View>
     );
-  }
+  };
 
   render() {
     return (
@@ -126,7 +160,7 @@ class HomeScreen extends Component {
         </View>
         <FlatList
           style={styles.feed}
-          data={posts}
+          data={this.state.listPost}
           renderItem={({ item }) => this.renderPost(item)}
           keyExtractor={(item) => item.id}
           showsVerticalScrollIndicator={false}
@@ -156,18 +190,19 @@ const styles = StyleSheet.create({
     zIndex: 10,
   },
   headerTitle: {
+    marginTop: 20,
     fontSize: 20,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   feed: {
     marginTop: 10,
     marginHorizontal: 16,
   },
   feedItem: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 5,
     padding: 8,
-    flexDirection: 'row',
+    flexDirection: "row",
     marginVertical: 8,
   },
   avatar: {
@@ -179,25 +214,25 @@ const styles = StyleSheet.create({
   name: {
     marginTop: 5,
     fontSize: 15,
-    fontWeight: '500',
-    color: '#454D65'
+    fontWeight: "500",
+    color: "#454D65",
   },
   timestamp: {
     fontSize: 11,
-    color: '#C4C6CE',
+    color: "#C4C6CE",
     marginTop: 4,
   },
   post: {
     marginTop: 16,
     fontSize: 14,
-    color: '#838899'
+    color: "#838899",
   },
-  postImage: {
+  image: {
     width: undefined,
     height: 150,
     borderRadius: 5,
     marginVertical: 16,
-  }
+  },
 });
 
 //make this component available to the app
